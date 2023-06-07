fd36d3400d1449ed8cb58776fe952239ec288360
Korakoe <56580073+korakoe@users.noreply.github.com>
2023-06-07 15:43:48 +0800

    why does this work

--- muse_maskgit_pytorch/trainers/vqvae_trainers.py
+++ muse_maskgit_pytorch/trainers/vqvae_trainers.py
@@ -101,7 +101,7 @@ class VQGanVAETrainer(BaseAcceleratedTrainer):
 
         # optimizers
         self.optim = get_optimizer(use_8bit_adam, optimizer, vae_parameters, lr, weight_decay)
-        self.discr_optim = get_optimizer(use_8bit_adam, optimizer, discr_parameters, lr, weight_decay)
+        self.discr_optim = get_optimizer(use_8bit_adam, optimizer, vae_parameters, lr, weight_decay)
         
         self.lr_scheduler: LRScheduler = get_scheduler(
             lr_scheduler_type,
