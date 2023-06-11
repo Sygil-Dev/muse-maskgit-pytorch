4baf63f997bcee1ba208383729a7aaf446f3ce2b
Alejandro Gil <ZeroCool940711@users.noreply.github.com>
2023-06-11 00:13:57 -0700

    Added hf_split_name to set the split or subset to use for huggingface datasets. - Renamed `skip_arrow` to `no_cache.`

--- train_muse_maskgit.py
+++ train_muse_maskgit.py
@@ -189,6 +189,12 @@ parser.add_argument(
     help="ID of HuggingFace dataset to use (cannot be used with --train_data_dir)",
 )
 parser.add_argument(
+    "--hf_split_name",
+    type=str,
+    default="train",
+    help="Subset or split to use from the dataset when using a dataset form HuggingFace.",
+)
+parser.add_argument(
     "--streaming",
     action="store_true",
     help="If using a HuggingFace dataset, whether to stream it or not.",
@@ -254,9 +260,9 @@ parser.add_argument(
     help="Image Size.",
 )
 parser.add_argument(
-    "--vq_codebook_dim", 
-    type=int, 
-    default=256, 
+    "--vq_codebook_dim",
+    type=int,
+    default=256,
     help="VQ Codebook dimensions.")
 parser.add_argument(
     "--cond_drop_prob",
@@ -335,9 +341,9 @@ parser.add_argument(
     help="The path to cache huggingface models",
 )
 parser.add_argument(
-    "--skip_arrow",
+    "--no_cache",
     action="store_true",
-    help="whether to skip converting the dataset to arrow, and to directly fetch data",
+    help="Do not save the dataset pyarrow cache/files to disk to save disk space and reduce the time it takes to launch the training.",
 )
 parser.add_argument(
     "--link",
@@ -425,7 +431,7 @@ class Arguments:
     optimizer: str = "Lion"
     weight_decay: float = 0.0
     cache_path: Optional[str] = None
-    skip_arrow: bool = False
+    no_cache: bool = False
     link: bool = False
     latest_checkpoint: bool = False
     debug: bool = False
@@ -491,15 +497,13 @@ def main():
     # Load the dataset (main process first to download, rest will load from cache)
     with accelerator.main_process_first():
         if args.train_data_dir is not None:
-            if args.skip_arrow:
-                pass
-            else:
-                dataset = get_dataset_from_dataroot(
-                    args.train_data_dir,
-                    image_column=args.image_column,
-                    caption_column=args.caption_column,
-                    save_path=args.dataset_save_path,
-                )
+            dataset = get_dataset_from_dataroot(
+                args.train_data_dir,
+                image_column=args.image_column,
+                caption_column=args.caption_column,
+                save_path=args.dataset_save_path,
+                save=not args.no_cache,
+            )
         elif args.dataset_name is not None:
             dataset = load_dataset(
                 args.dataset_name,
@@ -510,9 +514,9 @@ def main():
             )
             if args.streaming:
                 if args.cache_path:
-                    dataset = load_dataset(args.dataset_name, cache_dir=args.cache_path)["train"]
+                    dataset = load_dataset(args.dataset_name, cache_dir=args.cache_path)[args.hf_split_name]
                 else:
-                    dataset = load_dataset(args.dataset_name)["train"]
+                    dataset = load_dataset(args.dataset_name)[args.hf_split_name]
         else:
             raise ValueError("You must pass either train_data_dir or dataset_name (but not both)")
 
@@ -686,7 +690,7 @@ def main():
 
     # Create the dataset objects
     with accelerator.main_process_first():
-        if args.skip_arrow and args.train_data_dir:
+        if args.no_cache and args.train_data_dir:
             dataset = LocalTextImageDataset(
                 args.train_data_dir,
                 args.image_size,
