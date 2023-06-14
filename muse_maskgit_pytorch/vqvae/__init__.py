769fe2f807ad33b0af884a51eb255f04b36c005b
Andrew Powers-Holmes <aholmes@omnom.net>
2023-06-14 20:42:52 +1000

    add shiny VQVAE impl and test script

---
+++ muse_maskgit_pytorch/vqvae/__init__.py
@@ -0,0 +1,7 @@
+from .config import VQVAEConfig
+from .vqvae import VQVAE
+
+__all__ = [
+    "VQVAE",
+    "VQVAEConfig",
+]
---
+++ muse_maskgit_pytorch/vqvae/vqvae.py
@@ -0,0 +1,48 @@
+import logging
+
+import torch
+import torch.nn as nn
+from diffusers import ConfigMixin, ModelMixin
+from diffusers.configuration_utils import register_to_config
+
+from .layers import Decoder, Encoder
+from .quantize import VectorQuantize
+
+logger = logging.getLogger(__name__)
+
+
+class VQVAE(ModelMixin, ConfigMixin):
+    @register_to_config
+    def __init__(self, n_embed, embed_dim, beta, enc, dec, **kwargs):
+        super().__init__()
+        self.encoder = Encoder(**enc)
+        self.decoder = Decoder(**dec)
+
+        self.prev_quant = nn.Linear(enc["dim"], embed_dim)
+        self.quantize = VectorQuantize(n_embed, embed_dim, beta)
+        self.post_quant = nn.Linear(embed_dim, dec["dim"])
+
+    def freeze(self):
+        self.eval()
+        self.requires_grad_(False)
+
+    def encode(self, x):
+        x = self.encoder(x)
+        x = self.prev_quant(x)
+        x, loss, indices = self.quantize(x)
+        return x, loss, indices
+
+    def decode(self, x):
+        x = self.post_quant(x)
+        x = self.decoder(x)
+        return x.clamp(-1.0, 1.0)
+
+    def forward(self, img: torch.FloatTensor):
+        z, loss, indices = self.encode(img)
+        rec = self.decode(z)
+        return rec, loss
+
+    def decode_from_ids(self, indice):
+        z_q = self.quantize.get_codebook_entry(indice)
+        img = self.decode(z_q)
+        return img
