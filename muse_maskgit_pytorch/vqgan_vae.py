fce84eb2fa76c01d5f2446c5bb589fe608246fb1
Korakoe <56580073+korakoe@users.noreply.github.com>
2023-09-03 17:12:19 +0800

    Dumbest fix ever
    why?

--- muse_maskgit_pytorch/vqgan_vae.py
+++ muse_maskgit_pytorch/vqgan_vae.py
@@ -439,7 +439,7 @@ class VQGanVAE(nn.Module):
 
     @remove_vgg
     def load_state_dict(self, *args, **kwargs):
-        return super().load_state_dict(*args, **kwargs)
+        return super().load_state_dict(*args, **kwargs, strict=False)
 
     def save(self, path):
         if self.accelerator is not None:
